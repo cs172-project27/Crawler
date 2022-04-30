6023f1020b8c0f8ead0732805c933ea065db447f
Kashyap Panda <5233012+kashyappanda@users.noreply.github.com>
2022-04-30 02:55:33 -0700

    Initial commit

---
+++ tweepy-test.py
@@ -0,0 +1,12 @@
+import tweepy
+
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAGwqcAEAAAAAew5fSszXMJz4npuFgAeE7RVpUCo%3D8EB29IRC2vMKQxNOlL5hhJmlziqxMcl5hNPMPaCCGFIe7dZGRz"
+
+class CustomStreamingClient(tweepy.StreamingClient):
+    def on_tweet(self, tweet):
+        print(tweet.data)
+
+streaming_client = CustomStreamingClient(bearer_token)
+
+streaming_client.add_rules(tweepy.StreamRule("Elon Musk"))
+streaming_client.filter()
