ae2a5af8e7612394bb650ae8a3c02cb8f07c42a4
Kashyap Panda <5233012+kashyappanda@users.noreply.github.com>
2022-04-30 03:36:17 -0700

    Update tweepy-test.py

--- tweepy-test.py
+++ tweepy-test.py
@@ -1,14 +1,24 @@
 import tweepy
-import json
+import urllib
+import re
 
 bearer_token = "AAAAAAAAAAAAAAAAAAAAAGwqcAEAAAAAew5fSszXMJz4npuFgAeE7RVpUCo%3D8EB29IRC2vMKQxNOlL5hhJmlziqxMcl5hNPMPaCCGFIe7dZGRz"
 
+class Tweet:
+    def __init__(self, text, timestamp, geo, user, urls):
+        self.text = text
+        self.timestamp = timestamp
+        self.geo = geo
+        self.user = user
+        self.urls = urls
+
 class CustomStreamingClient(tweepy.StreamingClient):
     def on_tweet(self, tweet):
-        print(tweet.data)
+        urls = re.findall("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", tweet.text)
+        crawledTweet = Tweet(tweet.text, tweet.created_at, tweet.geo, tweet.author_id, urls)
+        print(crawledTweet.urls)
 
 streaming_client = CustomStreamingClient(bearer_token)
-
 
 filterRules = streaming_client.get_rules()[0]
 if filterRules is not None:
@@ -17,5 +27,5 @@ if filterRules is not None:
         rules_to_delete.append(rule.id)
     streaming_client.delete_rules(rules_to_delete)
 
-streaming_client.add_rules(tweepy.StreamRule("#DoctorStrange"))
+streaming_client.add_rules(tweepy.StreamRule("Elon Musk"))
 streaming_client.filter()
